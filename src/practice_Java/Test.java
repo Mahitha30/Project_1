2daad330870ebace2955e3e510ea3a7cb6b2af20
mahita@codewave.com <mahita@codewave.com>
2024-05-09 14:49:44 +0530

    1st commit

---
+++ src/practice_Java/Test.java
@@ -0,0 +1,31 @@
+package practice_Java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int i = 0;
+//		int j = i++ + ++i;
+//		System.out.println(j);
+		
+//		int i = 0;
+//		int j = 1;
+//		if(!i && j)
+//		{
+//			System.out.println("1");
+//		}
+//		else
+//		{
+//			System.out.println("2");
+//		}
+		
+//		boolean b1 = true, b2 =false, b3 = true;
+//		System.out.println(b1 && b2 && !b3);
+		
+		
+//		int  i = 10, j = 12, k = 1;
+//		k += i++ - --j;
+//		System.out.println(k);
+	}
+
+}
